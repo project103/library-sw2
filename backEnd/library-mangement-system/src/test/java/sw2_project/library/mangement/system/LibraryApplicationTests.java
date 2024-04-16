b0603acba5e3c8c1d78ad7a60671dbd284f906bf
abdallah <Bodivampire@yahoo.com>
2024-04-16 03:58:20 +0200

    abdallah
    نبتدي علي نضيف بقا

---
+++ backEnd/library-mangement-system/src/test/java/sw2_project/library/mangement/system/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package sw2_project.library.mangement.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
