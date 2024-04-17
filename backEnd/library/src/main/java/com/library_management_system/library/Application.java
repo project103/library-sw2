c7bafba149f4bf5fe5453ce69f01a3d59f73c318
abdallah <Bodivampire@yahoo.com>
2024-04-17 15:24:25 +0200

    Update Application.java

--- backEnd/library/src/main/java/com/library_management_system/library/Application.java
+++ backEnd/library/src/main/java/com/library_management_system/library/Application.java
@@ -2,8 +2,10 @@ package com.library_management_system.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class Application {
 
 	public static void main(String[] args) {
