64aab4bbceb457fef6efdf40a127aa77cd5de4ca
alaahishammm <alaahisham122@gmail.com>
2024-04-16 20:02:44 -0700

    alaa
    failed to start solution

--- backEnd/library-mangement-system/src/main/java/sw2_project/library/mangement/system/LibraryApplication.java
+++ backEnd/library-mangement-system/src/main/java/sw2_project/library/mangement/system/LibraryApplication.java
@@ -2,13 +2,16 @@ package sw2_project.library.mangement.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
-		//andrew
+		System.out.println("hello");
+
 	}
 
 }
