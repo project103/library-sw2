a50c6675cc54c0849d303b6a61d81de85e4ce795
Asherif02 <andrew_20210190@fci.helwan.edu.eg>
2024-04-19 01:51:00 +0200

    updates

--- backEnd/library/src/main/java/com/library_management_system/library/controller/userController.java
+++ backEnd/library/src/main/java/com/library_management_system/library/controller/userController.java
@@ -3,15 +3,8 @@ package com.library_management_system.library.controller;
 import com.library_management_system.library.service.userService;
 import com.library_management_system.library.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
@@ -51,8 +44,9 @@ public class userController {
     public User UpdateUser(@RequestBody User user) {
         return Service.UpdateUser(user);
     }
-@GetMapping("/login")
- public boolean login(@RequestBody Map<String,String> name) {
-    return Service.getUserByusername(name.get("name"),name.get("password"));
-}
+
+    @GetMapping("/login")
+    public boolean login(@RequestBody Map<String, String> name) {
+        return Service.getUserByusername(name.get("name"), name.get("password"));
+    }
 }
