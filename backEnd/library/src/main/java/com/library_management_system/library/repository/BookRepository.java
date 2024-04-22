a47125d19fcc72abea96727525e588457195cf94
abdallah <Bodivampire@yahoo.com>
2024-04-22 20:08:26 +0200

    Update BookRepository.java

--- backEnd/library/src/main/java/com/library_management_system/library/repository/BookRepository.java
+++ backEnd/library/src/main/java/com/library_management_system/library/repository/BookRepository.java
@@ -1,13 +1,16 @@
 package com.library_management_system.library.repository;
 
 import com.library_management_system.library.entity.Book;
-import com.library_management_system.library.entity.CartItem;
+import com.library_management_system.library.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
     Book getBookById(int id);
 
     Book getBookByname(String bookId);
+    List<Book> findBookByCategory(Category category);
 
     // You can add more custom query methods as needed
 }
