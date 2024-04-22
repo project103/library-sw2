6ddeb5b14d19c9c034e9e7bef187a1e9de403770
abdallah <Bodivampire@yahoo.com>
2024-04-22 20:00:14 +0200

    Abdallah

--- backEnd/library/src/main/java/com/library_management_system/library/repository/BookRepository.java
+++ backEnd/library/src/main/java/com/library_management_system/library/repository/BookRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookRepository extends JpaRepository<Book, Integer> {
     Book getBookById(int id);
 
+    Book getBookByname(String bookId);
+
     // You can add more custom query methods as needed
 }
